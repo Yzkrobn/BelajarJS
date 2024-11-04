d27bec4faabf5772cc926402aa8663f57d751634
Yzkrobn <oktavianbagasnugroho@gmailcom>
2024-11-04 15:05:52 +0700

    Memperbarui desain responsif dengan Tailwind CSS

--- public/js/script.js
+++ public/js/script.js
@@ -1,31 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const nB = document.getElementById("navButton");
-    const nS = document.getElementById("navSide");
-    const l1 = document.getElementById("line1");
-    const l2 = document.getElementById("line2");
-    const l3 = document.getElementById("line3");
-    const navSlide = () => {
-        if (nB && nS) {
-            nB.addEventListener('click', () => {
-                nS.classList.toggle('translate-x-20')
-                nS.classList.toggle('opacity-0');     // Toggle opacity antara 0 dan 100
-                nS.classList.toggle('invisible');
-                // nS.classList.toggle('hidden');
-            })
-        }
+document.addEventListener("DOMContentLoaded", () => {
+  const nB = document.getElementById("navButton");
+  const nC = document.getElementById("navController");
+  const l1 = document.getElementById("line1");
+  const l2 = document.getElementById("line2");
+  const l3 = document.getElementById("line3");
+  const nS = document.getElementById("navSide");
+
+  const navSide = () => {
+    if (nB && nC && l1 && l2 && l3) {
+      nB.addEventListener("click", () => {
+        nC.classList.toggle("opacity-0");
+        nC.classList.toggle("invisible");
+        l1.classList.toggle("rotate-45");
+        l1.classList.toggle("-translate-y-0.5");
+        l3.classList.toggle("-rotate-45");
+        l2.classList.toggle("opacity-0");
+        l2.classList.toggle("invisible");
+      });
     }
-    navSlide();
-    const rotate = () => {
-        if (nB && l1 && l2 && l3) {
-            nB.addEventListener('click', () => {
-                l1.classList.toggle("rotate-45")
-                l1.classList.toggle("-translate-y-3")
-                l2.classList.toggle("-rotate-45")
-                l2.classList.toggle("-translate-y-4")
-                l3.classList.toggle('opacity-0');     // Toggle opacity antara 0 dan 100
-                l3.classList.toggle('invisible');
-            })
-        }
-    }
-    rotate()
-})
+  };
+  navSide();
+});
